886bc67d60ee3c0216205db6ab8d1990cea3d179
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-10-01 10:38:39 +0800

    Finished the palindrome problem.

--- leetcode/LC125_isPalindrome/index.js
+++ leetcode/LC125_isPalindrome/index.js
@@ -1,3 +1,20 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+    // sanitize input string
+    s = s.toLowerCase().replace(/[\W_]/g, '');
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while(left < right) {
+        if(s[left] !== s[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
 
 module.exports = isPalindrome;
