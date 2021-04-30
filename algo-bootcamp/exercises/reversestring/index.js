454dc101bf34894fadd84d263af0730e5ca77dc5
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-30 23:42:34 +0800

    Finished the reverse string.

--- algo-bootcamp/exercises/reversestring/index.js
+++ algo-bootcamp/exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
+
+// solution 1
+// function reverse(str) {
+//   return str.split('')
+//     .reverse()
+//     .join();
+// }
+
+// solution 2
+// function reverse(str) {
+//   let reversed = '';
+
+//   for(let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
