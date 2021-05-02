8e068cb4a709c85eb1499e24937c83b7aa8dad83
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-05-02 09:42:05 +0800

    Finished the reverse int.

--- algo-bootcamp/exercises/reverseint/index.js
+++ algo-bootcamp/exercises/reverseint/index.js
@@ -8,6 +8,40 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n.toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// My Solution
+// function reverseInt(n) {
+//   let signResult = Math.sign(n);
+//
+//   if (signResult === 0) {
+//     return 0;
+//   }
+//
+//   // convert number to array
+//   let numArr = n.toString()
+//     .split('');
+//
+//   if (signResult === 1) {
+//     let reversed = numArr.reverse()
+//       .join('');
+//
+//     return parseInt(reversed);
+//   }
+//
+//   // pluck the first character
+//   numArr.shift();
+//   let reversed = '-' + numArr.reverse()
+//     .join('');
+//
+//   return parseInt(reversed);
+// }
