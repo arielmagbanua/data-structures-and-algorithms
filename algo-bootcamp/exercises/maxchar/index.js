bb7ae338adc076dd55328d25131aee2ebb23103a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-05-02 10:23:43 +0800

    Finished the max chars.

--- algo-bootcamp/exercises/maxchar/index.js
+++ algo-bootcamp/exercises/maxchar/index.js
@@ -5,6 +5,51 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+// my solution
+// function maxChar(str) {
+//   let charRef = {};
+//
+//   for (let character of str) {
+//     if (!charRef[character]) {
+//       charRef[character] = 0;
+//     }
+//
+//     ++charRef[character];
+//   }
+//
+//   let chosenKey = '';
+//   let count = 0;
+//   for (let [key, value] of Object.entries(charRef)) {
+//     if (count < value) {
+//       count = value;
+//       chosenKey = key;
+//     }
+//   }
+//
+//   return chosenKey;
+// }
