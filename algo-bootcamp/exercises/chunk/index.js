7cf40b673692a0383d73cdd8ef15d861e75e5784
unknown <magbanua.c.ariel@gmail.com>
2021-09-05 10:51:27 +0800

    Finished chunked array.

--- algo-bootcamp/exercises/chunk/index.js
+++ algo-bootcamp/exercises/chunk/index.js
@@ -8,6 +8,46 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunks  = [];
+
+    // let temp = [];
+
+    // for(let item of array) {
+    //     if (temp.length === size) {
+    //         // add the chunks
+    //         chunks.push(temp);
+    //         // reset
+    //         temp = [];
+    //     }
+
+    //     temp.push(item);
+    // }
+
+    // if (temp.length > 0) {
+    //     // add the remaining chunk
+    //     chunks.push(temp);
+    //     temp = [];
+    // }
+
+    // for(let item of array) {
+    //     const last = chunks[chunks.length - 1];
+
+    //     if (!last || last.length === size) {
+    //         chunks.push([item]);
+    //     } else {
+    //         last.push(item);
+    //     }
+    // }
+
+    let index = 0;
+
+    while (index < array.length) {
+        chunks.push(array.slice(index, index + size));
+        index += size;
+    }
+
+    return chunks;
+}
 
 module.exports = chunk;
