a0e989f8eec5a2efa604d9969153ebfed2fe4e4f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-09-03 01:09:31 +0800

    Finished palindrome

--- AlgoCasts/exercises/palindrome/index.js
+++ AlgoCasts/exercises/palindrome/index.js
@@ -7,6 +7,22 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// solution 1
+// function palindrome(str) {
+//   let rev = '';
+
+//   for (let char of str) {
+//     rev = char + rev;
+//   }
+
+//   return rev === str;
+// }
+
+// solution 2
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
+}
 
 module.exports = palindrome;
