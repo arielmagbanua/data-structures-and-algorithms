9b5952cbd88e1ef7a09851d0b76035e570462e9f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-09-04 01:01:29 +0800

    Solved the max char

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  let greatestKey = null;
+  let greatestValue = 0;
+  for (const [key, value] of Object.entries(charMap)) {
+    if (value > greatestValue) {
+      greatestKey = key;
+      greatestValue = value;
+    }
+  }
+
+  return greatestKey;
+}
 
 module.exports = maxChar;
